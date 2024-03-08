d4cb45bb2ffc977a16a9bb2828e11934b43854e7
Petar Mihaylov <petar.m@studio350.com>
2024-03-08 13:48:16 -0500

    Fix auto version increment 2

--- .husky/post-commit-task.sh
+++ .husky/post-commit-task.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Fetch the latest tag
-latestTag="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 # Break down the version number into its components
 regex="([0-9]+).([0-9]+).([0-9]+)"
-if expr "$latestTag" : "$regex" > /dev/null; then
-major=$(expr "$latestTag" : 'v\([0-9]*\)')
-minor=$(expr "$latestTag" : 'v[0-9]*\.\([0-9]*\)\.[0-9]*')
-patch=$(echo "$latestTag" | sed -E 's/.*\.([0-9]+)$/\1/')
+if [[ $latestTag =~ $regex ]]; then
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  patch="${BASH_REMATCH[3]}"
 fi
 
 # Increment the patch version
@@ -16,9 +16,9 @@ newPatch=$((patch + 1))
 newTag="v$major.$minor.$newPatch"
 
 # Create the new tag
-git tag "$newTag"
+git tag $newTag
 
 # Optional: Push the new tag to the remote repository
-git push origin "$newTag"
+git push origin $newTag
 
 echo "New tag $newTag created and pushed"
