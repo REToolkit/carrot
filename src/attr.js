d73b8710940503b4dab6fb8a2159b62d06ab1351
Petar Mihaylov <petar.m@studio350.com>
2024-03-08 13:38:28 -0500

    Fix logo or link

--- src/attr.js
+++ src/attr.js
@@ -1,79 +1,80 @@
-$(document).ready(function() {
-    // Function to retrieve the script tag's custom attribute value
-    function getScriptOption() {
-        // Get all script tags
-        var scripts = document.getElementsByTagName('script');
-        var currentScript = scripts[scripts.length - 1]; // Current script is the last one
+(function($) {
+    $(document).ready(function() {
+        // Function to retrieve the script tag's custom attribute value
+        function getScriptOption() {
+            // Get all script tags
+            var scripts = document.getElementsByTagName('script');
+            var currentScript = scripts[scripts.length - 1]; // Current script is the last one
 
-        // Read the 'data-option' attribute
-        return currentScript.getAttribute('data-option');
-    }
+            // Read the 'data-option' attribute
+            return currentScript.getAttribute('data-option');
+        }
 
-    const option = getScriptOption(); // 'div' or 'link'
-    const hrefText = 'REToolkit - SEO for Real Estate Investing';
-    const baseURL = window.location.hostname;
-    const baseHref = 'https://retoolkit.io/?utm_source=' + encodeURIComponent(baseURL);
+        const option = getScriptOption(); // 'div' or 'link'
+        const hrefText = 'REToolkit - SEO for Real Estate Investing';
+        const baseURL = window.location.hostname;
+        const baseHref = 'https://retoolkit.io/?utm_source=' + encodeURIComponent(baseURL);
 
-    // Function to create and append the responsive div with image and link
-    function createResponsiveDiv() {
-        var responsiveDiv = $('<div/>', {
-            css: {
-                width: '100%',
-                maxWidth: '600px',
-                margin: '0 auto'
-            }
-        });
+        // Function to create and append the responsive div with image and link
+        function createResponsiveDiv() {
+            var responsiveDiv = $('<div/>', {
+                css: {
+                    width: '100%',
+                    maxWidth: '600px',
+                    margin: '0 auto'
+                }
+            });
 
-        var imageRow = $('<div/>', {
-            css: {
-                width: '100%',
-                maxWidth: '200px',
-                marginBottom: '20px'
-            }
-        }).appendTo(responsiveDiv);
+            var imageRow = $('<div/>', {
+                css: {
+                    width: '100%',
+                    maxWidth: '200px',
+                    marginBottom: '20px'
+                }
+            }).appendTo(responsiveDiv);
 
-        $('<img/>', {
-            src: 'https://global.divhunt.com/b6840b489b614157d3f82de68e8da0d9_10914.webp',
-            alt: hrefText,
-            css: {
-                width: '100%',
-                height: 'auto'
-            }
-        }).appendTo(imageRow);
+            $('<img/>', {
+                src: 'https://global.divhunt.com/b6840b489b614157d3f82de68e8da0d9_10914.webp',
+                alt: hrefText,
+                css: {
+                    width: '100%',
+                    height: 'auto'
+                }
+            }).appendTo(imageRow);
 
-        var linkRow = $('<div/>', {
-            css: {
-                width: '100%'
-            }
-        }).appendTo(responsiveDiv);
+            var linkRow = $('<div/>', {
+                css: {
+                    width: '100%'
+                }
+            }).appendTo(responsiveDiv);
 
-        $('<a/>', {
-            href: baseHref,
-            text: hrefText
-        }).appendTo(linkRow);
+            $('<a/>', {
+                href: baseHref,
+                text: hrefText
+            }).appendTo(linkRow);
 
-        $('footer .disclaimer').append(responsiveDiv);
-    }
+            $('footer .disclaimer').append(responsiveDiv);
+        }
 
-    // Function to create and append just the attribution link
-    function createAttributionLink() {
-        var attributionText = $('<span>', {
-            text: ' | '
-        });
+        // Function to create and append just the attribution link
+        function createAttributionLink() {
+            var attributionText = $('<span>', {
+                text: ' | '
+            });
 
-        var attributionLink = $('<a>', {
-            href: baseHref,
-            text: hrefText
-        });
+            var attributionLink = $('<a>', {
+                href: baseHref,
+                text: hrefText
+            });
 
-        $('footer .copy').append(attributionText).append(attributionLink);
-    }
+            $('footer .copy').append(attributionText).append(attributionLink);
+        }
 
-    // Decide which content to insert based on the 'data-option' attribute
-    if (option === 'div') {
-        createResponsiveDiv();
-    } else if (option === 'link') {
-        createAttributionLink();
-    }
-});
-
+        // Decide which content to insert based on the 'data-option' attribute
+        if (option === 'div') {
+            createResponsiveDiv();
+        } else if (option === 'link') {
+            createAttributionLink();
+        }
+    });
+})(jQuery);
